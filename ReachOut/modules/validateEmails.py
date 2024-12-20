f62e8cb23ab567e8f18f74fe51eaf08f771dfacd
Shashank Reddy <reddy@Shashanks-MacBook-Air.local>
2024-12-19 22:26:37 -0800

    pre-directory name change

---
+++ ReachOut/modules/validateEmails.py
@@ -0,0 +1,13 @@
+from validate_email import validate_email
+
+class emailRecruiter:
+
+    def __init__():
+        pass
+
+    def check_email(from_addr, to_addr):
+        is_valid = validate_email(
+            email_address=to_addr,
+            smtp_from_address=from_addr)
+        
+    
